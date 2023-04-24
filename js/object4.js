0ae0c99fcdd44982c48f43238efd37fcb7d9df99
Mike Frager <mfrager@gmail.com>
2023-04-24 16:09:05 -0700

    Update objects

--- js/object4.js
+++ js/object4.js
@@ -27,54 +27,33 @@ async function main() {
             { url: 'https://www.gravatar.com/avatar/c9cb338a29d608d33e16ff3f2e7f9635?s=64&d=identicon&r=PG' },
         ],
         isAccessibleForFree: true,
+        funder: [
+            {
+                type: 'IPerson',
+                givenName: 'Some',
+                familyName: 'Body',
+            },
+            {
+                type: 'IOrganization',
+                name: 'BigCo',
+            },
+        ],
     }
     //console.log(obj)
 
     const store = new N3.Store()
     builder.buildResource(store, type, obj.id, obj, {})
     const jsobj = await graphToJsonld(store, baseUri)
-
-    /*const jstxt = JSON.stringify(jsobj)
+    const jstxt = JSON.stringify(jsobj, null, 4)
+    console.log(jstxt)
+    
     const graph = await jsonldToGraph(jstxt)
     const mainId = builder.getUriForUUID(graph, obj.uuid)
-    console.log(mainId)
-    const mainType = builder.getType(graph, mainId)
+    //console.log(mainId)
     if (mainId) {
-        const jsres = builder.decodeResource(graph, mainType, mainId, {})
+        const jsres = builder.decodeResource(graph, mainId, {})
         console.log(jsres)
-    }*/
-    
-
-    const apiBase = 'http://173.234.24.74:9500'
-    const lc = new ListingClient(provider, catalogProgram, apiBase)
-    const listingSpec = lc.getListingSpec({
-        catalog: 'commerce',
-        base: 'http://173.234.24.74:9500/api/catalog/listing/',
-        category: 'http://schema.org/Event',
-        label: obj.name,
-        detail: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "Event",
-        }),
-        attributes: [
-            'InPerson',
-        ],
-        locality: [
-            'https://www.geonames.org/6252001/', // United States
-            'https://www.geonames.org/5332921/', // California
-            'https://www.geonames.org/5350736/', // Fremont
-        ],
-        owner: provider.wallet.publicKey,
-    })
-    //console.log(listingSpec)
-    const li = await lc.getListingInstructions(listingSpec, provider.wallet.publicKey)
-    //console.log(li)
-    //console.log('Store Record')
-    //console.log(await lc.storeRecord(user, obj.uuid, jsobj))
-    //console.log('Store Listing')
-    //console.log(await lc.storeListing(user, obj.uuid, li.catalog, li.uuid))
-    console.log(await lc.storeRecordAndListing(user, obj.uuid, jsobj, li.catalog, li.uuid))
-    console.log(await lc.sendListingInstructions(li))
+    }
 }
 
 console.log('Begin')
